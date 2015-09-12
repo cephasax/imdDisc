2011b3e1e212a1c7d125c355373f49e4b9ea67d2
victorvilar <victor_vilar@hotmail.com>
2015-09-12 20:04:35 -0300

    Domains and pages

---
+++ br/ufrn/imd/web2/projeto1/Disciplina.java
@@ -0,0 +1,9 @@
+package br.ufrn.imd.web2.projeto1;
+
+public class Disciplina {
+
+	public Disciplina() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ br/ufrn/imd/web2/projeto1/Pessoa.java
@@ -0,0 +1,9 @@
+package br.ufrn.imd.web2.projeto1;
+
+public class Pessoa {
+
+	public Pessoa() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
