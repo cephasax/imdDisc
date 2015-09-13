54c67639fb8f4eccd8149b3dca93638798ea30c8
cephasax <cephasax@hotmail.com>
2015-09-12 21:57:25 -0300

    Megazord adds
    adicionados getters and setters no Megazord

---
+++ JavaSource/br/ufrn/imd/web2/projeto1/MegazordBean.java
@@ -0,0 +1,49 @@
+package br.ufrn.imd.web2.projeto1;
+
+import java.util.ArrayList;
+
+public class MegazordBean {
+
+	public Disciplina disciplina;
+	public Pessoa pessoa;
+	public ArrayList<Disciplina> listaDisciplinas;
+	public ArrayList<Pessoa> listaPessoas;
+
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public ArrayList<Disciplina> getListaDisciplinas() {
+		return listaDisciplinas;
+	}
+
+	public void setListaDisciplinas(ArrayList<Disciplina> listaDisciplinas) {
+		this.listaDisciplinas = listaDisciplinas;
+	}
+
+	public ArrayList<Pessoa> getListaPessoas() {
+		return listaPessoas;
+	}
+
+	public void setListaPessoas(ArrayList<Pessoa> listaPessoas) {
+		this.listaPessoas = listaPessoas;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+	}
+	
+}
